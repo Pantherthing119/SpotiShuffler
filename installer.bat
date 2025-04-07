6c7b6f97d0bc7db686efc5c01e9aa7da9abc479d
Sam Brereton <samuel.brereton@outlook.com>
2025-04-07 21:25:21 +0100

    added option to launch spotiShuffler after install. Installer may be finished

--- installer.bat
+++ installer.bat
@@ -1,5 +1,12 @@
 @echo off
-COPY ".\SpotiShuffler.lnk" "%AppData%\Microsoft\Windows\Start Menu"
-echo hellooooo
-echo another line haha
-pause
+py -m pip install spotipy
+py -m pip install Pillow
+cd /D "%~dp0"
+XCOPY ".\SpotiShuffler.lnk" "%AppData%\Microsoft\Windows\Start Menu" /D
+echo.
+echo.
+echo Installation complete
+echo.
+set /P askLaunch=Do you want to launch SpotiShuffler [Y/N]?
+if %askLaunch% == Y pyw ".\spotiShuffler.pyw"
+exit
