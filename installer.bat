b70ea3ba34aadaa0557f53cedda03d6297d96247
unknown <samuel.brereton@outlook.com>
2025-06-08 22:13:51 +0100

    changed start menu path and changed to pause rather than exit after execution is complete

--- installer.bat
+++ installer.bat
@@ -2,11 +2,11 @@
 py -m pip install spotipy
 py -m pip install Pillow
 cd /D "%~dp0"
-XCOPY ".\SpotiShuffler.lnk" "%AppData%\Microsoft\Windows\Start Menu" /D
+XCOPY ".\SpotiShuffler.lnk" "%AppData%\Microsoft\Windows\Start Menu\Programs" /D
 echo.
 echo.
 echo Installation complete
 echo.
 set /P askLaunch=Do you want to launch SpotiShuffler [Y/N]?
 if %askLaunch% == Y start SpotiShuffler.lnk
-exit
+pause
